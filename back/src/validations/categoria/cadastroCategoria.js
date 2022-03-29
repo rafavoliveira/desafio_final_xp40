f1172f3a45431eda8c0a495234512c112af312a3
Mateus Sousa <mateusmaciel340@gmail.com>
2022-03-28 21:15:14 -0300

    Adicionando lógica de validação do campo foto

--- back/src/validations/categoria/cadastroCategoria.js
+++ back/src/validations/categoria/cadastroCategoria.js
@@ -2,6 +2,7 @@ const { validate, Joi } = require("express-validation");
 
 module.exports = validate({
   body: Joi.object({
+    fotoCategoria: Joi.string().required(),
     nomeCategoria: Joi.string().required(),
     descricaoCategoria: Joi.string().required(),
   }),
--- back/src/validations/produto/cadastroProduto.js
+++ back/src/validations/produto/cadastroProduto.js
@@ -2,6 +2,7 @@ const { validate, Joi } = require("express-validation");
 
 module.exports = validate({
   body: Joi.object({
+    fotoProduto: Joi.string().required(),
     nomeProduto: Joi.string().required(),
     descricaoProduto: Joi.string().required(),
     categoriaId: Joi.number().required(),
